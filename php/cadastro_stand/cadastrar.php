7539d60ecf7f4d94eb7eaba799159d51c66cc882
Fafas <fabriciohipvpaul@gmail.com>
2025-05-27 20:08:36 -0300

    Inserção no Banco

--- php/cadastro_stand/cadastrar.php
+++ php/cadastro_stand/cadastrar.php
@@ -14,30 +14,49 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
       );
     $camImgBD= 'uploads/' . $novoNome; //Caminho pro banco ler flagra
 
+//# IMPORTANTE # - Provavelmente vai ter que arrumar esse caminho posteriormente
 
 
-    //IMPORTA O RESTO DAS INFOS DO STAND
+
+//IMPORTA O RESTO DAS INFOS DO STAND------------------------------------------------------------------
 
     $nome = $_POST['nome_stand'];
     $tipo = $_POST['tipoStand'];
 
-    $poder = $_POST['poderDestrutivo'];
-    $velocidade = $_POST['velocidade'];
-    $alcance = $_POST['alcance'];
-    $persistencia = $_POST['persistencia'];
-    $precisao = $_POST['precisao'];
-    $potencial = $_POST['potencial'];
 
-    $nmhab1 = $_POST['nmhab1'];
-    $hab1 = $_POST['hab1'];
-    $nvl1 = $_POST['nvlhab1'];
-    $nmhab2 = $_POST['nmhab2'];
-    $hab2 = $_POST['hab2'];
-    $nvl2 = $_POST['nvlhab2'];
+//ATRIBUTOS STAND-------------------------------------------------------------------------------------
+    $map_atr = [
+    'nulo' => 0,
+    'e' => 1,
+    'd' => 2,
+    'c' => 3,
+    'b' => 4,
+    'a' => 5,   
+    'infinito' => 6, 
+    ];
+
+    function mapValor($valor, $map_atr) {
+    return $map_atr[$valor];
+    }
+
+    $poder = mapValor($_POST['poderDestrutivo'], $map_atr);
+    $velocidade = mapValor($_POST['velocidade'], $map_atr);
+    $alcance = mapValor($_POST['alcance'], $map_atr);
+    $persistencia = mapValor($_POST['persistencia'], $map_atr);
+    $precisao = mapValor($_POST['precisao'], $map_atr);
+    $potencial = mapValor($_POST['potencial'], $map_atr);
+ 
+//HABILIDADES---------------------------------------------------------------------------------------------------------
+
+    $nmhab = $_POST['nmhab'];
+    $hab = $_POST['hab'];
+    $nvl = $_POST['nvlhab'];
 
     $raridade = $_POST['raridade'];
     $parte = $_POST['parte'];
 
+
+//ECHO DE DEBUG---------------------------------------------------------------------------------------------------------
     echo "
         <strong>Stand cadastrado com sucesso!</strong><br>
         <strong>Nome:</strong> $nome <br>
@@ -49,18 +68,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <strong>Precisão:</strong> $precisao <br>
         <strong>Potencial:</strong> $potencial <br><br>
 
-        <strong>Habilidade 1:</strong> $nmhab1 (Nível: $nvl1)<br>
-        <em>$hab1</em><br><br>
+        <strong>Habilidade 1:</strong> {$nmhab[0]} (Nível: {$nvl[0]})<br>
+        <em>{$hab[0]}</em><br><br>
 
-        <strong>Habilidade 2:</strong> $nmhab2 (Nível: $nvl2)<br>
-        <em>$hab2</em><br><br>
+        <strong>Habilidade 2:</strong> {$nmhab[1]} (Nível: {$nvl[1]})<br>
+        <em>{$hab[1]}</em><br><br>
 
         <strong>Raridade:</strong> $raridade <br>
         <strong>Parte:</strong> $parte <br>
         <strong>Imagem salva como:</strong> $camImgBD
     ";
 
-    
+//INSERÇÃO NO BANCO---------------------------------------------------------------------------------------------------------
+      
       $sql = "INSERT INTO `heroi`(
           `Nome`, 
           `Imagem`, 
@@ -70,7 +90,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
           `Velocidade`, 
           `Alcance`, 
           `Persistencia_Poder`, 
-          `Precisao`, 
+          `Precisao`,
+          `Potencial`,
           `universo`
       ) VALUES (
           :nome, 
@@ -81,18 +102,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
           :velocidade, 
           :alcance, 
           :persistencia_poder, 
-          :precisao, 
+          :precisao,
+          :potencial, 
           :universo
       )";
 
-    
+      $sqlhabs = "INSERT INTO `habilidade`(`Nome`, `Descricao`, `Lv_habilidade`) VALUES (:nmhab,:hab,:nvl)";
 
     
     $cadastrar_stand = $conn->prepare($sql);
-
-
-    $cadastrar_hab_stand = $conn->prepare($sql2);
-
+    $cadastrar_habilidade = $conn->prepare($sqlhabs);
 
     try {
       $cadastrar_stand->execute(array(  
@@ -105,10 +124,23 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
       "alcance" => $alcance,
       "persistencia_poder" => $persistencia,
       "precisao" => $precisao,
+      "potencial" => $potencial,
       "universo" => $parte
-    ))
-    } catch (\Throwable $th) {
-      //throw $th;
-    };
+      ));
+    } catch (PDOException $e) {
+      echo "Erro ao cadastrar stand: " . $e->getMessage();
+    }
 
+    try {
+      for ($i=0; $i < count($nmhab); $i++) { 
+      $cadastrar_habilidade->execute(array(  
+      "nmhab" => $nmhab[$i],
+      "hab" => $hab[$i],
+      "nvl" => $nvl[$i]
+      ));
+      }
+    } catch (PDOException $e) {
+      echo "Erro ao cadastrar habilidade: " . $e->getMessage();
+    }
 }
+
